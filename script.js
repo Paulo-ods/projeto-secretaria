f99ed758ae75b2b78de5a05bc52546dca835d6e1
Paulo José dos Santos <paulo-santos_07@hotmail.com>
2025-11-03 22:30:32 -0300

    news section

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-  const menuBtn = document.getElementById("menuBtn");
-  const mobileMenu = document.getElementById("mobileMenu");
+    document.getElementById("leadForm").addEventListener("submit", function(e) {
+      e.preventDefault();
+      alert("Obrigada por se inscrever! Entraremos em contato em breve.");
+      this.reset();
+    });
 
-  menuBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-  });
+    const menuBtn = document.getElementById("menuBtn");
+    const mobileMenu = document.getElementById("mobileMenu");
+
+    menuBtn.addEventListener("click", () => {
+        mobileMenu.classList.toggle("hidden");
+    });
